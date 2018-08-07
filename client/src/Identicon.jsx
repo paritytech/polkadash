6457ae5d7248ba42c8ec0d2efe50a8cd7f8426b3
Gav <gavin@parity.io>
2018-08-07 09:53:47 +0200

    Improvements to identicon

--- client/src/Identicon.jsx
+++ client/src/Identicon.jsx
@@ -28,35 +28,29 @@ export default class Identicon extends ReactiveComponent {
 			</svg>)
 		} else {
 			let z = s / 64 * 5
-			// solid    2   1
-			// flower   4   4
-			// asterisc 7   4
-			// adidas   7   7
-			// 3cyclic  7   7
-			// hmirror 11  11
-			// vmirror 12  12
-			// --------------
-			//             46
 			let schema = {
-				solid: 1,
-				flower: 4,
-				asterisk: 4,
-				adidas: 7,
-				"3cyclic": 7,
-				hmirror: 11,
-				vmirror: 12
+				solid: { freq: 1, colors: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1] },
+				flower: { freq: 4, colors: [0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 3] },
+				asterisk: { freq: 4, colors: [0, 0, 1, 0, 0, 2, 0, 0, 3, 0, 0, 4, 0, 0, 5, 0, 0, 6, 0] },
+				cube: { freq: 16, colors: [0, 1, 3, 2, 4, 3, 0, 1, 3, 2, 4, 3, 0, 1, 3, 2, 4, 3, 5] },
+				quazar: { freq: 16, colors: [1, 2, 3, 1, 2, 4, 5, 5, 4, 1, 2, 3, 1, 2, 4, 5, 5, 4, 0] },
+				adidas: { freq: 32, colors: [0, 1, 0, 0, 1, 1, 2, 3, 2, 2, 3, 3, 4, 5, 4, 4, 5, 5, 6] },
+				cyclic: { freq: 32, colors: [0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 6] },
+				vmirror: { freq: 128, colors: [0, 1, 2, 3, 4, 5, 3, 4, 2, 0, 1, 6, 7, 8, 9, 7, 8, 6, 10] },
+				hmirror: { freq: 128, colors: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 8, 6, 7, 5, 3, 4, 2, 11] }
 			}
+			let total = Object.keys(schema).map(k => schema[k].freq).reduce((a, b) => a + b)
 			let findScheme = d => {
 				let cum = 0
 				let ks = Object.keys(schema)
 				for (let i in ks) {
-					let n = schema[ks[i]]
+					let n = schema[ks[i]].freq
 					cum += n;
 					if (d < cum) {
-						return ks[i]
+						return schema[ks[i]]
 					}
 				}
-				throw "Invalid"
+				throw "Impossible"
 			}
 			
 			let id = this.state.id
@@ -64,112 +58,38 @@ export default class Identicon extends ReactiveComponent {
 				return <svg width={s} height={s}/>
 			}
 			let sat = id[29] * 80 / 256 + 20
-			let d = Math.floor((id[30] + id[31] * 256) * 46 / 65536)
-			let type = findScheme(d)
+			let d = Math.floor((id[30] + id[31] * 256) % total)
+			let scheme = findScheme(d)
 			let palette = Array.from(id).map(b => {
 				if (b == 0) {
 					return 'transparent'
 				}
 				let h = b % 64 * 360 / 64
-				let l = b / 64 * 100 / 4
+				let l = b / 64 * 70 / 4 + 15
 				return `hsl(${h}, ${sat}%, ${l}%)`
 			})
-
-			let scheme = { type, palette }
-
-			let colors = (() => { switch (scheme.type) {
-				case 'solid': return [
-					scheme.palette[0], scheme.palette[0], scheme.palette[0],
-					scheme.palette[0], scheme.palette[0], scheme.palette[0],
-					scheme.palette[0], scheme.palette[0], scheme.palette[0],
-					scheme.palette[0], scheme.palette[0], scheme.palette[0],
-					scheme.palette[0], scheme.palette[0], scheme.palette[0],
-					scheme.palette[0], scheme.palette[0], scheme.palette[0],
-					scheme.palette[1]
-				]
-				case '3cyclic': return [
-					scheme.palette[0], scheme.palette[1], scheme.palette[2],
-					scheme.palette[3], scheme.palette[4], scheme.palette[5],
-					scheme.palette[0], scheme.palette[1], scheme.palette[2],
-					scheme.palette[3], scheme.palette[4], scheme.palette[5],
-					scheme.palette[0], scheme.palette[1], scheme.palette[2],
-					scheme.palette[3], scheme.palette[4], scheme.palette[5],
-					scheme.palette[6]
-				]
-				case 'flower': return [
-					scheme.palette[0], scheme.palette[1], scheme.palette[2],
-					scheme.palette[0], scheme.palette[1], scheme.palette[2],
-					scheme.palette[0], scheme.palette[1], scheme.palette[2],
-					scheme.palette[0], scheme.palette[1], scheme.palette[2],
-					scheme.palette[0], scheme.palette[1], scheme.palette[2],
-					scheme.palette[0], scheme.palette[1], scheme.palette[2],
-					scheme.palette[3]
-				]
-				case 'asterisk': return [
-					scheme.palette[0], scheme.palette[0], scheme.palette[1],
-					scheme.palette[0], scheme.palette[0], scheme.palette[2],
-					scheme.palette[0], scheme.palette[0], scheme.palette[3],
-					scheme.palette[0], scheme.palette[0], scheme.palette[4],
-					scheme.palette[0], scheme.palette[0], scheme.palette[5],
-					scheme.palette[0], scheme.palette[0], scheme.palette[6],
-					scheme.palette[0]
-				]
-				case 'adidas': return [
-					scheme.palette[0], scheme.palette[1], scheme.palette[0],
-					scheme.palette[0], scheme.palette[1], scheme.palette[1],
-					scheme.palette[2], scheme.palette[3], scheme.palette[2],
-					scheme.palette[2], scheme.palette[3], scheme.palette[3],
-					scheme.palette[4], scheme.palette[5], scheme.palette[4],
-					scheme.palette[4], scheme.palette[5], scheme.palette[5],
-					scheme.palette[6]
-				]
-				case 'hmirror': return [
-					scheme.palette[0], scheme.palette[1], scheme.palette[2],
-					scheme.palette[3], scheme.palette[4], scheme.palette[5],
-					scheme.palette[3], scheme.palette[4], scheme.palette[2],
-					scheme.palette[0], scheme.palette[1], scheme.palette[6],
-					scheme.palette[7], scheme.palette[8], scheme.palette[9],
-					scheme.palette[7], scheme.palette[8], scheme.palette[6],
-					scheme.palette[10]
-				]
-				case 'vmirror': return [
-					scheme.palette[0], scheme.palette[1], scheme.palette[2],
-					scheme.palette[3], scheme.palette[4], scheme.palette[5],
-					scheme.palette[6], scheme.palette[7], scheme.palette[8],
-					scheme.palette[9], scheme.palette[10], scheme.palette[8],
-					scheme.palette[6], scheme.palette[7], scheme.palette[5],
-					scheme.palette[3], scheme.palette[4], scheme.palette[2],
-					scheme.palette[11]
-				]
-			}})()
+			let colors = scheme.colors.map(i => palette[i])
 			let i = 0;
 			return (<svg width={s} height={s}>
-				<circle cx={s / 2} cy={s / 2} r={s / 2} fill="#fff"/>
-
-				<circle cx={c - r} cy={c} r={z} fill={colors[i++]}/>
-				<circle cx={c - ro2} cy={c} r={z} fill={colors[i++]}/>
-				<circle cx={c - r3o4} cy={c - rroot3o4} r={z} fill={colors[i++]}/>
-
-				<circle cx={c - ro2} cy={c - rroot3o2} r={z} fill={colors[i++]}/>
-				<circle cx={c - ro4} cy={c - rroot3o4} r={z} fill={colors[i++]}/>
-				<circle cx={c} cy={c - rroot3o2} r={z} fill={colors[i++]}/>
-
-				<circle cx={c + ro2} cy={c - rroot3o2} r={z} fill={colors[i++]}/>
-				<circle cx={c + ro4} cy={c - rroot3o4} r={z} fill={colors[i++]}/>
-				<circle cx={c + r3o4} cy={c - rroot3o4} r={z} fill={colors[i++]}/>
-
-				<circle cx={c + r} cy={c} r={z} fill={colors[i++]}/>
-				<circle cx={c + ro2} cy={c} r={z} fill={colors[i++]}/>
-				<circle cx={c + r3o4} cy={c + rroot3o4} r={z} fill={colors[i++]}/>
-
-				<circle cx={c + ro2} cy={c + rroot3o2} r={z} fill={colors[i++]}/>
-				<circle cx={c + ro4} cy={c + rroot3o4} r={z} fill={colors[i++]}/>
-				<circle cx={c} cy={c + rroot3o2} r={z} fill={colors[i++]}/>
-
-				<circle cx={c - ro2} cy={c + rroot3o2} r={z} fill={colors[i++]}/>
-				<circle cx={c - ro4} cy={c + rroot3o4} r={z} fill={colors[i++]}/>
-				<circle cx={c - r3o4} cy={c + rroot3o4} r={z} fill={colors[i++]}/>
-
+				<circle cx={s / 2} cy={s / 2} r={s / 2} fill="#eee"/>
+				<circle cx={c} cy={c - r} r={z} fill={colors[i++]}/>
+				<circle cx={c} cy={c - ro2} r={z} fill={colors[i++]}/>
+				<circle cx={c - rroot3o4} cy={c - r3o4} r={z} fill={colors[i++]}/>
+				<circle cx={c - rroot3o2} cy={c - ro2} r={z} fill={colors[i++]}/>
+				<circle cx={c - rroot3o4} cy={c - ro4} r={z} fill={colors[i++]}/>
+				<circle cx={c - rroot3o2} cy={c} r={z} fill={colors[i++]}/>
+				<circle cx={c - rroot3o2} cy={c + ro2} r={z} fill={colors[i++]}/>
+				<circle cx={c - rroot3o4} cy={c + ro4} r={z} fill={colors[i++]}/>
+				<circle cx={c - rroot3o4} cy={c + r3o4} r={z} fill={colors[i++]}/>
+				<circle cx={c} cy={c + r} r={z} fill={colors[i++]}/>
+				<circle cx={c} cy={c + ro2} r={z} fill={colors[i++]}/>
+				<circle cx={c + rroot3o4} cy={c + r3o4} r={z} fill={colors[i++]}/>
+				<circle cx={c + rroot3o2} cy={c + ro2} r={z} fill={colors[i++]}/>
+				<circle cx={c + rroot3o4} cy={c + ro4} r={z} fill={colors[i++]}/>
+				<circle cx={c + rroot3o2} cy={c} r={z} fill={colors[i++]}/>
+				<circle cx={c + rroot3o2} cy={c - ro2} r={z} fill={colors[i++]}/>
+				<circle cx={c + rroot3o4} cy={c - ro4} r={z} fill={colors[i++]}/>
+				<circle cx={c + rroot3o4} cy={c - r3o4} r={z} fill={colors[i++]}/>
 				<circle cx={c} cy={c} r={z} fill={colors[i++]}/>
 			</svg>)
 		}
